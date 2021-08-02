6211a792b47ac175463a337cbcee5680e986ac93
Dani <dani8439@aol.com>
2021-08-02 13:48:42 -0400

    completed challenge for Variables and Values lecture.

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,7 @@
+let country = "United States of Amerca";
+let continent = "North America";
+let population = "382.2 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
