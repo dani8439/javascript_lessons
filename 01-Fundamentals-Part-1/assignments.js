dda4f582af81736acf1f04d99398060d62b129a0
Dani <dani8439@aol.com>
2021-08-03 11:49:41 -0400

    assignment on const and let.

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,14 +1,16 @@
-// let country = "United States of Amerca";
-// let continent = "North America";
-// let population = "382.2 million";
+const country = "United States of Amerca";
+const continent = "North America";
+let population = "382.2 million";
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// let isIsland = false;
-// let language;
-// console.log(isIsland);
-// console.log(population);
-// console.log(country);
-// console.log(language);
+const isIsland = false;
+const language = "English";
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+language = "Spanish";
