aaf1cba2ab61be8dbf51f1a9baeb240a495fb4fa
Dani <dani8439@aol.com>
2021-08-04 13:59:20 -0400

    Notes and examples for if/else statements.

--- 01-Fundamentals-Part-1/index.js
+++ 01-Fundamentals-Part-1/index.js
@@ -157,6 +157,7 @@ console.log(markHigherBMI);
 // console.log(markWeight, markHeight, johnWeight, johnHeight);
 */
 
+/*
 const firstName = "Jonas";
 const job = "teacher";
 const birthYear = 1991;
@@ -179,3 +180,25 @@ lines");
 console.log(`String
 multiple
 lines`);
+*/
+
+const age = 15;
+// const isOldEnough = age >= 18;
+
+//command control space to get to emojis.
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
---
+++ 01-Fundamentals-Part-1/notes7.md
@@ -0,0 +1,24 @@
+# Taking Decisions: if / else statements
+
+```
+if (conditionWeAreChecking)
+    if condition evaluates as true then {
+    block within curly braces is executed.
+    }
+```
+
+else block is optional.
+
+```
+    if (condition) {
+
+    } else {
+
+    }
+```
+
+if/else statement is called a control structure. Let's us have more control over the way our code is executed.
+
+JavaScript code doesn't execute linearly. We control over what is controlled and when each block runs.
+
+Any code declared inside of the block, is not accessible outside of the block.
