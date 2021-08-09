e6d628d92a273e4c640794a64b362fd255f2e09c
Dani <dani8439@aol.com>
2021-08-09 13:26:04 -0400

    updated folders.

--- 01-Fundamentals-Part-1/index.js
+++ 01-Fundamentals-Part-1/index.js
@@ -518,9 +518,9 @@ HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 GOOD LUCK 😀
 */
 
-const bill = 430;
+// const bill = 40;
 
-const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-console.log(
-  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
-);
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+// );
