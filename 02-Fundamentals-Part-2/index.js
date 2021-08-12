26babbd177b2c672cfa838d427ce08823cd7c759
Dani <dani8439@aol.com>
2021-08-12 09:57:19 -0400

    For Loops.

--- 02-Fundamentals-Part-2/index.js
+++ 02-Fundamentals-Part-2/index.js
@@ -379,6 +379,8 @@ TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 GOOD LUCK 😀
 */
 
+/*
+
 const mark = {
   fullName: "Mark Miller",
   mass: 78,
@@ -414,4 +416,22 @@ if (mark.bmi > john.bmi) {
   console.log(
     `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`
   );
+}
+*/
+
+// violates don't repeat yourself pricinple.
+// console.log("Lifting weights repetition 1 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 2 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 3 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 4 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 5 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 6 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 7 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 8 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 9 🏋🏻‍♀️");
+// console.log("Lifting weights repetition 10 🏋🏻‍♀️");
+
+// for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋🏻‍♀️`);
 }
---
+++ 02-Fundamentals-Part-2/notes12.md
@@ -0,0 +1,12 @@
+# Iteration: The For Loop
+
+Loops are a control structure. (Just like if/else statements)
+
+Fundamental aspect of programming, as they allow us to automate repetitive tasks.
+
+## For Loop
+
+for(counter; condition; )
+loop statement has 3 parts, 1st part is initial value of the counter, declared with let.
+2nd part is logical condition evaluated before each iteration of the loop. As soon as that condition is false, the loop stops.
+3rd part is the counter, and how it updates after each iteration, otherwise would always stay at 1.
