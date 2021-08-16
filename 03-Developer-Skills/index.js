ce3a9fc4b1c168f3301fceef982868517d73192c
Dani <dani8439@aol.com>
2021-08-16 14:33:05 -0400

    Debugging with the console and breakpoints.

--- 03-Developer-Skills/index.js
+++ 03-Developer-Skills/index.js
@@ -4,6 +4,8 @@
 // PROBLEM 1
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
+/*
+
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
 // 1) Understanding the problem.
@@ -67,3 +69,51 @@ console.log(amplitudeNew);
 const array1 = ["a", "b", "c"];
 const array2 = ["d", "e", "f"];
 const array3 = array1.concat(array2);
+*/
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    // C) FIX
+    // value: Number(prompt("Degrees celsius:"))
+    value: 10
+  };
+
+  // B) FIND THE BUG
+  console.table(measurement);
+
+  //   console.log(measurement.value);
+  //   console.warn(measurement.value);
+  //   console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+// A) IDENTIFY THE BUG
+console.log(measureKelvin());
+
+// Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  //   debugger;
+  // BUG -- Won't loop through for min as 1 can't be less than 0
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENIFY
+console.log(amplitudeBug);
