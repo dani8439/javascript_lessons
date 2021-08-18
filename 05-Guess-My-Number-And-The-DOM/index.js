bf0dd2a16a86a464582f51f5d6307c49c26b50a7
Dani <dani8439@aol.com>
2021-08-18 15:29:22 -0400

    Implementing High Scores.

--- 05-Guess-My-Number-And-The-DOM/index.js
+++ 05-Guess-My-Number-And-The-DOM/index.js
@@ -15,6 +15,7 @@ document.querySelector('.guess').value = 23;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 // score can be called a state variable, held in application state, all data relevant to the application.
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -33,6 +34,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // when guess is too high
   } else if (guess > secretNumber) {
