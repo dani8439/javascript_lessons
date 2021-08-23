ed7cde1f37fe798f2a80e7eb8b58ffb4e11985d6
Dani <dani8439@aol.com>
2021-08-23 15:52:57 -0400

    Pig Game, set up.

--- 07-Pig-Game/index.js
+++ 07-Pig-Game/index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// specifying numbers not strings, but JS will automatically convert to strings to display on page
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
---
+++ 07-Pig-Game/notes.md
@@ -0,0 +1,7 @@
+# Pig Game
+
+Another way to select elements by id rather than using `document.querySelector('#id')`
+
+instead use: `document.getElementById('id')`
+
+Don't need the hash, only passing in name of ID we're looking for. `getElementById` supposed to be a little faster than `querySelector`
