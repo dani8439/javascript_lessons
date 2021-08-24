f4f8c7f076a2c98b8308fd4055e53a691994cddd
Dani <dani8439@aol.com>
2021-08-24 15:59:48 -0400

    Resetting the game.

--- 07-Pig-Game/index.js
+++ 07-Pig-Game/index.js
@@ -16,17 +16,31 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// specifying numbers not strings, but JS will automatically convert to strings to display on page
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-// state variable
-let playing = true;
+// specifying numbers not strings, but JS will automatically convert to strings to display on page
+
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  // state variable
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   // if active player is 0, then switch to player 1. If not, switch to player 0.
@@ -96,3 +110,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
+// do not call the function, JS will call it as soon as user clicks on new button.
---
+++ 07-Pig-Game/notes4.md
@@ -0,0 +1,4 @@
+# Implementing New Game Button
+
+Want to remove winner class when click on new game, and set all scores back to 0.
+Need to setback all initial conditions of the game.
