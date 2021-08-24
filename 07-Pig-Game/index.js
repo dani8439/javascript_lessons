a061e7a7e85850523cf94496a48307690a781063
Dani <dani8439@aol.com>
2021-08-24 14:15:36 -0400

    Switching active player.

--- 07-Pig-Game/index.js
+++ 07-Pig-Game/index.js
@@ -1,8 +1,13 @@
 'use strict';
 
 // Selecting elements
+// selects the background to shift color
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+// selects the store
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+// selects the current player
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
@@ -17,7 +22,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -34,8 +41,19 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    // select score element dynamically based on who the active player is now.
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // switch to next player
+    // Switch to next player
+    // if active player is 0, then switch to player 1. If not, switch to player 0.
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    // toggle will add the class if not there, if it is there, it'll remove it.
+    // toggling both at the same time will make sure it's only on one element at once.
+    // toggles back and forth between active player background - shifts color on the page
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
--- 07-Pig-Game/notes2.md
+++ 07-Pig-Game/notes2.md
@@ -1,3 +1,8 @@
 # Rolling the Dice
 
 Want to react to clicking the roll button. Need to select that button, and add an event listener/handler
+
+## Switching from one active player to another
+
+When dice is not a 1, it's one player. When it is a 1, shift to logic of second block in if/else statement.
+Need to keep track of which player is the current player. (or the active player when dice was rolled)
