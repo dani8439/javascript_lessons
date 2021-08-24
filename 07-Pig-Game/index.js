862eaa330cc1ebe456534a55936f684de70ab9c4
Dani <dani8439@aol.com>
2021-08-24 15:25:12 -0400

    holding the current score.

--- 07-Pig-Game/index.js
+++ 07-Pig-Game/index.js
@@ -25,35 +25,74 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+// state variable
+let playing = true;
+
+const switchPlayer = function () {
+  // if active player is 0, then switch to player 1. If not, switch to player 0.
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  // toggle will add the class if not there, if it is there, it'll remove it.
+  // toggling both at the same time will make sure it's only on one element at once.
+  // toggles back and forth between active player background - shifts color on the page
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. Generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  // sees to it that so long as game is active, can use the buttons
+  if (playing) {
+    // 1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //   console.log(dice);
 
-  // 2. Display the dice
-  diceEl.classList.remove('hidden');
-  // dynamically load one of the images depending on the randomly rolled dice.
-  diceEl.src = `dice-${dice}.png`;
+    // 2. Display the dice
+    diceEl.classList.remove('hidden');
+    // dynamically load one of the images depending on the randomly rolled dice.
+    diceEl.src = `dice-${dice}.png`;
 
-  // 3. Check for a rolled 1: if true, switch to next player
-  if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
-    // select score element dynamically based on who the active player is now.
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    // Switch to next player
-    // if active player is 0, then switch to player 1. If not, switch to player 0.
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    // toggle will add the class if not there, if it is there, it'll remove it.
-    // toggling both at the same time will make sure it's only on one element at once.
-    // toggles back and forth between active player background - shifts color on the page
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    // 3. Check for a rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      // select score element dynamically based on who the active player is now.
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // Switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //   console.log('Hold button');
+    // 1. Add current score to active player's score.
+    // when it's player 1, it's scores1 in the scores array.
+    scores[activePlayer] += currentScore;
+    //   scores[1] = scores[1] + currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is already >=100.
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      // adds back the class so dice is hidden if someone wins.
+      diceEl.classList.add('hidden');
+      // Finish game if it is.
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
---
+++ 07-Pig-Game/notes3.md
@@ -0,0 +1,8 @@
+# Implementing player holding the current score.
+
+Implementing the game into smaller problems through flow chart. Makes things easier.
+
+Functionality of holding the score, whenever user clicks on the button, it holds the score. When that happens, want to add the current score to the total score. At the moment, score resets.
+
+Switching of the player only happens when score is below 100.
+Want something to happen when user clicks on `btnHold`
