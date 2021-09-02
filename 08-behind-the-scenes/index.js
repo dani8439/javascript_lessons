9b9249141b9810ceca7358793d2aedef8aeacff1
Dani <dani8439@aol.com>
2021-09-02 14:40:44 -0400

    Scoping in practice.

--- 08-behind-the-scenes/index.js
+++ 08-behind-the-scenes/index.js
@@ -1,36 +1,47 @@
 'use strict';
 
-// const name = 'Jonas';
+// defined in global scope. Here in top level code. Function creates it's own scope, equivalent to variable environment of it's execution context
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
 
-// const first = () => {
-//   let a = 1;
-//   const b = second(7, 9);
-//   a = a + b;
-//   return a;
-// };
+  function printAge() {
+    // can't find age in current scope. Goes up one, and finds it in the variable we created in teh parent scope. Same thing goes for birthYear.
+    // does variable lookup for firstName too, to the global level.
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
 
-// function second(x, y) {
-//   var c = 2;
-//   return c;
-// }
+    // block scope
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // JS always look for variable in current scope. So can define a variable that's already been defined in another scope. Won't throw an error. Won't look it up. Outside of the block scope, though it's still from the parent scope. In fact, they are completely different variables, that happen to have the same name. Also why you can have functions with the same parameter names.
+      // creating NEW variable with same name as outer scope's variable.
+      const firstName = 'Steven';
 
-// const x = first();
+      // reassigning outer scope's variable.
+      output = 'NEW OUTPUT';
 
-const myName = 'Jonas';
+      const str = `Oh, and you're a millenail, ${firstName}`;
+      console.log(str);
 
-function first() {
-  const age = 30;
-
-  if (age >= 30) {
-    // true
-    const decade = 3;
-    var millenial = true;
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // error logging it outside of the block. const and let variables are block scoped. Only available inside block they are created. Not true with var. var variables don't care about blocks at all.
+    // console.log(str);
+    console.log(millenial);
+    // functions are also block scoped. Get error when calling add function outside of the block. Only true for strict mode though.
+    // console.log(add(2, 3));
+    // can see it, because manipulated an existing variable within the child scope. If we had created it as a const, then it would be same situation as firstName. It would be a new output variable, that just happens to have the same name. The console.log below, would revert to the original output.
+    console.log(output);
   }
-
-  function second() {
-    const job = 'teacher';
-
-    console.log(`${myName} is a ${age}-old ${job}`);
-    // Jonas is a 30-year old teacher
-  }
+  printAge();
+  return age;
 }
+
+// global variable
+const firstName = 'Jonas';
+calcAge(1991);
+// cannot access it outside of the function. Scope is one way, out in, not inner to outer. Same thing goes for printAge(). No access to variables defined in any other scope, in the global scope
+// console.log(age);
+// printAge();
