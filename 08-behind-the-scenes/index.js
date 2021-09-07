7eca479ee017a2f02d38cda76252bab2a8fb1226
Dani <dani8439@aol.com>
2021-09-07 15:47:55 -0400

    Hoisting and TDZ practice.

--- 08-behind-the-scenes/index.js
+++ 08-behind-the-scenes/index.js
@@ -48,11 +48,72 @@ calcAge(1991);
 // printAge();
 */
 
-const myName = 'Jonas';
-if (myName == 'Jonas') {
-  console.log(`Jonas is a ${job}`);
-  const age = 2037 - 1989;
-  console.log(age);
-  const job = 'teacher';
-  console.log(x);
+// const myName = 'Jonas';
+// if (myName == 'Jonas') {
+//   console.log(`Jonas is a ${job}`);
+//   const age = 2037 - 1989;
+//   console.log(age);
+//   const job = 'teacher';
+//   console.log(x);
+// }
+
+// Hoisting with Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+// results in undefined. Var values are hoisted, but hoisted to undefined
+let job = 'teacher';
+// let, cannot access Uncaught ReferenceError. in the TDZ
+// TDZ starts from beginning of the scope, until where it's defined. so from line 61 to line 67.
+// Get the same error for const below.
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// we actually get the result of 5. Able to call the function declaration before it was defined in the code.
+// console.log(addExpr(2, 3));
+// Cannot access addExpr before it's initialized. With a function expression. As it's a const variable too, and in the TDZ. Simply assigning a function value to this variable. As the variable was defined with const, in a TDZ.
+// console.log(addArrow(2, 3));
+// Same happens with the arrow.
+console.log(addArrow); // undefined
+
+function addDecl(a, b) {
+  return a + b;
 }
+
+// if change both from const to var, still get an error, but different error UncaughtType Error. Any variables declared with var will be hoisted and set to undefined. changing functions to var, you are then trying to call undefined(2,3) then get told, it's not a function. Have changed addExpr to an undefined variable essentially. Told it's not a function.
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// only function you can use before declaring is a function declaration.
+
+// Example
+// What should happen, is that as numProducts = 10, that's not a falsy value, so deleteShoppingCart() shouldn't be called. Except, because of hoisting, and defininig with var, var is initialized as undefined, which is a falsy value, so that then evaluates, triggers the execution of the if block, and calls the deleteShoppingCart() function.
+
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+/// conclusion as to a best practice, DON'T USE VAR to declare variables. Use CONST and let when necessary. Should declare variables at the top of each scope. Finally, always declare your functions first, and only use after the declarations. Use it this way for all types of functions. This way is cleaner, and best practices.
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+// testing if x is a property of the window object. And it is.
+console.log(y === window.y); // false
+console.log(z === window.z); // false
+
+// window is the global object of JS in the browser
+// when declare with var, we can then find our declared variable on the window object. Doesn't happen with let and const.
