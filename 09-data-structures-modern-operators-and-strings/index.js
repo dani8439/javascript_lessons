98fda9f8aa95dd70f6f0da88d9ca8c5dcdfc58ba
Dani <dani8439@aol.com>
2021-10-07 12:06:07 -0400

    setting up for rest patterns and parameters notes and code.

--- 09-data-structures-modern-operators-and-strings/index.js
+++ 09-data-structures-modern-operators-and-strings/index.js
@@ -144,6 +144,7 @@ console.log(p, q, r);
 // } = openingHours;
 // console.log(o, c);
 
+/*
 // Spread Operator
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -197,3 +198,4 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+*/
