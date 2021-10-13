f774a101ef4bbdb9b2d264b6e7555e757cb31a21
Dani <dani8439@aol.com>
2021-10-13 11:41:56 -0400

    reverse strings of 5 characters or more solution.

--- codewars_katas/index.js
+++ codewars_katas/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+/// Stop gninnipS My sdroW!
+function spinWords(str) {
+  // splits the string on spaces
+  let strArr = str.split(" ");
+  console.log(strArr);
+  for (let i = 0; i < strArr.length; i++) {
+    // when the stringArrays are 5 characters or more, split on the spaces, reverse the characters, and then join back together
+    if (strArr[i].length >= 5)
+      strArr[i] = strArr[i].split("").reverse().join("");
+    console.log(strArr);
+  }
+  return strArr.join(" ");
+}
