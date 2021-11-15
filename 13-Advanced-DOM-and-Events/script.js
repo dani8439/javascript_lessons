0a5da4f12ca1055512d8a8a5d8fea991a6e10f66
Dani <dani8439@aol.com>
2021-11-15 10:49:11 -0500

    Types of events and event handlers.

--- 13-Advanced-DOM-and-Events/script.js
+++ 13-Advanced-DOM-and-Events/script.js
@@ -158,3 +158,21 @@ btnScrollTo.addEventListener('click', function (e) {
   // new way to do it without any of the weird calculations works the same
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+/////////////////
+// Events
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+
+  // h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = function (e) {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
