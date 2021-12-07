6a09a20638dd689eb3c6500bd9e68c18a6f59241
Dani <dani8439@aol.com>
2021-12-07 10:57:48 -0500

    JavaScriptmas Day 7

---
+++ scrimba_javascriptmas/day7/index.js
@@ -0,0 +1,24 @@
+const playBtn = document.getElementById("play-btn");
+const pauseBtn = document.getElementById("pause-btn");
+const stopBtn = document.getElementById("stop-btn");
+
+// Task:
+// - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
+
+const bells = new Audio("bells.mp3");
+
+playBtn.addEventListener("click", () => {
+  bells.play();
+});
+
+pauseBtn.addEventListener("click", () => {
+  bells.pause();
+});
+
+stopBtn.addEventListener("click", () => {
+  bells.pause();
+});
+
+// Stretch goals:
+// - Add volume controls.
+// - Allow the user to select different sounds.
