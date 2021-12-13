d82e8eb0525b63b029625e92e83d64abe3a681ff
Dani <dani8439@aol.com>
2021-12-13 11:00:21 -0500

    Updated day 10 javascriptmas with scrimba solution

--- scrimba_javascriptmas/day10/index.js
+++ scrimba_javascriptmas/day10/index.js
@@ -1,18 +1,25 @@
 const meter = document.getElementById("meter");
+const today = new Date().getDate();
 
 // Task:
 // Write a function to wire up the festivity loader to reflect how many days are remaining until Christmas!
 
-function showChristmas() {
-  const christmas = new Date("December 25, 2021");
-  const c = christmas.getDate();
-  const today = new Date();
-  const t = today.getDate();
-  meter.value = c - t;
-  /// to get the percentage it should be ((c - t)/ 25) * 100
+function displayMeterValue() {
+  meter.setAttribute("value", today);
 }
 
-showChristmas();
+displayMeterValue();
+
+// function showChristmas() {
+//   const christmas = new Date("December 25, 2021");
+//   const c = christmas.getDate();
+//   const today = new Date();
+//   const t = today.getDate();
+//   meter.value = c - t;
+//   /// to get the percentage it should be ((c - t)/ 25) * 100
+// }
+
+// showChristmas();
 
 // Stretch goals:
 // - Animate the loader.
