b49407523efcfaa3c7f6b3cf2e7c5d7a2fa9b539
Dani <dani8439@aol.com>
2021-12-14 14:14:49 -0500

    updated

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -127,3 +127,11 @@ const jonas = new Person('Jonas');
 console.log('Jonas' ?? null);
 
 console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+// import 'core-js/stable';
+// import 'core-js/stable/array/find';
+// import 'core-js/stable/promise';
+
+// // Polyfilling async functions
+// import 'regenerator-runtime/runtime';
