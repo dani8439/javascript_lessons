347acd5694406569554dc6e6b914478580da78c5
Dani <dani8439@aol.com>
2021-12-24 14:02:28 -0500

    Setting up

---
+++ scrimba_javascriptmas/day20/index.js
@@ -0,0 +1,33 @@
+const inputs = document.querySelectorAll(".controls input");
+
+// Task:
+// Write a function to update the snowman colors according to the colors selected from the pickers.
+
+// Stretch goals:
+// - Add other items eg scarf, arms, etc.
+// - Add different options for nose shape, or hats.
+// - Check for contrast between pupils and eye color.
+
+//
+//////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+//  const inputs = document.querySelectorAll(".controls input");
+
+//  function handleUpdate() {
+//    document.documentElement.style.setProperty(`--${this.name}`, this.value);
+//  }
+
+//  inputs.forEach((input) => input.addEventListener("change", handleUpdate));
